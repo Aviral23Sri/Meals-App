1fc905328c80e06a6b1face9526553879b20ab74
Aviral Srivastava <kanha23srivastava@gmail.com>
2024-08-24 14:43:35 +0530

    Adding a Logic and working of the favoriteMeals Section, added a metadata like ingrediantrs steps to meals details screen , added a tabs section to toggle between the categories screen and favorites screen, added a appDrawer to the app , used ListTile to add the elements in the drawer,

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:meals/data/dummy_data.dart';
+// import 'package:meals/data/dummy_data.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/screens/categories.dart';
-import 'package:meals/screens/meals.dart';
+// import 'package:meals/screens/categories.dart';
+// import 'package:meals/screens/meals.dart';
+import 'package:meals/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
+    seedColor: Color.fromARGB(255, 34, 128, 182),
   ),
   textTheme: GoogleFonts.latoTextTheme(),
 );
@@ -24,7 +25,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home:const CategoriesScreen(),
+      home:const TabsScreen(),
     );
   }
 }
--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,21 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+
 class MealDetailsScreen extends StatelessWidget {
-  const MealDetailsScreen({required this.meal, super.key});
+  const MealDetailsScreen({
+    required this.meal,
+    super.key,
+    required this.onToggleFavorite,
+  });
 
   final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
+        actions: [
+          IconButton(
+
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
+            icon: const Icon(Icons.favorite),
+          ),
+        ],
       ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              width: double.infinity,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 15),
+              ),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold),
+            ),
+            for (final steps in meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                child: Text(
+                  steps,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontSize: 15,
+                      ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
